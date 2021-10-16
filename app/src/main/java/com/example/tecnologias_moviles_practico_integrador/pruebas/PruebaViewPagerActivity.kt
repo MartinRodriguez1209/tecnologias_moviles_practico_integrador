49bc5314d3e771129322e82eca9854f2161a979b
MartinRodriguez1209 <martinrodriguez.1209@gmail.com>
2021-10-16 19:20:51 -0300

    intento de viewPager con glide, todavia no funcion " java.lang.StackOverflowError: stack size 8192KB"

---
+++ app/src/main/java/com/example/tecnologias_moviles_practico_integrador/pruebas/PruebaViewPagerActivity.kt
@@ -0,0 +1,35 @@
+package com.example.tecnologias_moviles_practico_integrador.pruebas
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tecnologias_moviles_practico_integrador.R
+import androidx.viewpager.widget.ViewPager
+import com.example.tecnologias_moviles_practico_integrador.qr.visor.viewpager.ViewPagerAdapter
+
+
+class PruebaViewPagerActivity : AppCompatActivity() {
+
+    lateinit var mViewPager: ViewPager
+    private val urls = arrayOf(
+        "https://ichef.bbci.co.uk/news/640/cpsprodpb/150EA/production/_107005268_gettyimages-611696954.jpg",
+        "https://cdn.britannica.com/q:60/91/181391-050-1DA18304/cat-toes-paw-number-paws-tiger-tabby.jpg",
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Cat_November_2010-1a.jpg/767px-Cat_November_2010-1a.jpg",
+        "https://steamuserimages-a.akamaihd.net/ugc/1661229473514719956/AA7CB1965DB65B7F07CF9B08E84CE5F0B0CCE0D0/?imw=637&imh=358&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true",
+    )
+    lateinit var viewpageadapter: ViewPagerAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_prueba_view_pager)
+        mViewPager = findViewById(R.id.view_pager_prueba)
+        viewpageadapter = ViewPagerAdapter(this, urls)
+        mViewPager.adapter = viewpageadapter
+
+        var viewpager = findViewById<ViewPager>(R.id.view_pager_prueba)
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/tecnologias_moviles_practico_integrador/qr/visor/viewpager/ViewPagerAdapter.kt
@@ -0,0 +1,58 @@
+package com.example.tecnologias_moviles_practico_integrador.qr.visor.viewpager
+
+
+import android.content.Context;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import androidx.viewpager.widget.PagerAdapter;
+import com.bumptech.glide.Glide
+import com.example.tecnologias_moviles_practico_integrador.R
+import com.example.tecnologias_moviles_practico_integrador.pruebas.PruebaViewPagerActivity
+import java.util.Objects;
+
+
+class ViewPagerAdapter(pruebaViewPager: PruebaViewPagerActivity, urls: Array<String>) :
+    PagerAdapter() {
+
+
+    private var context = pruebaViewPager.applicationContext
+    private var images = urls
+    private lateinit var imageView: ImageView
+    private var mLayoutInflater =
+        context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+
+    override fun getCount(): Int {
+        return images.size
+    }
+
+    override fun isViewFromObject(view: View, `object`: Any): Boolean {
+        return view === `object` as LinearLayout
+    }
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        // inflating the item.xml
+        val itemView = mLayoutInflater.inflate(R.layout.item_prueba, container, false)
+
+        // referencing the image view from the item.xml file
+        imageView = itemView.findViewById<View>(R.id.imageView_prueba) as ImageView
+
+        // setting the image in the imageView
+        Glide.with(context).load(images[position]).into(imageView)
+
+
+        Objects.requireNonNull(container).addView(itemView)
+        return itemView!!
+    }
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        destroyItem(container, position, `object`)
+        Glide.with(context).clear(imageView)
+        Glide.get(context).clearMemory()
+    }
+
+}
